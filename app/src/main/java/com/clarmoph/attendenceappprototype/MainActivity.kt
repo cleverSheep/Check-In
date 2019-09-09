ea4e6303071e60235d59b5040a6b3f08f2ed6211
cleverSheep <clarmoph1029@gmail.com>
2019-09-08 21:43:16 -0400

    remove viewpager paging

--- app/src/main/java/com/clarmoph/attendenceappprototype/MainActivity.kt
+++ app/src/main/java/com/clarmoph/attendenceappprototype/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.clarmoph.attendenceappprototype
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.clarmoph.attendenceappprototype.adapters.PagerAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mViewPager: ViewPager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mPagerAdapter = PagerAdapter(supportFragmentManager)
+        mViewPager = findViewById(R.id.proto_viewpager)
+        mViewPager.adapter = mPagerAdapter
+    }
+
+    override fun onBackPressed() {
+        if (mViewPager.currentItem == 0) {
+            super.onBackPressed()
+        } else {
+            mViewPager.currentItem = mViewPager.currentItem - 1
+        }
     }
 }
---
+++ app/src/main/java/com/clarmoph/attendenceappprototype/adapters/PagerAdapter.kt
@@ -0,0 +1,26 @@
+package com.clarmoph.attendenceappprototype.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.clarmoph.attendenceappprototype.views.AdjustCheckInTimeFragment
+import com.clarmoph.attendenceappprototype.views.AdjustProximityFragment
+
+class PagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    companion object PagerAdapter {
+        val NUM_IITEMS = 2
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> AdjustProximityFragment.newInstance()
+            1 -> AdjustCheckInTimeFragment.newInstance()
+            else -> null
+        }!!
+    }
+
+    override fun getCount(): Int {
+        return NUM_IITEMS
+    }
+}
