b909befec8d49b1b0504d8467cdacfd0727752cc
Murray <murrayde17@students.ecu.edu>
2019-09-09 16:15:42 -0400

    finish onboarding

--- app/src/main/java/com/clarmoph/attendenceappprototype/MainActivity.kt
+++ app/src/main/java/com/clarmoph/attendenceappprototype/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.clarmoph.attendenceappprototype.adapters.PagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,9 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setUpViewPager()
+    }
+
+    private fun setUpViewPager() {
         val mPagerAdapter = PagerAdapter(supportFragmentManager)
         mViewPager = findViewById(R.id.proto_viewpager)
         mViewPager.adapter = mPagerAdapter
+        tab_indicator.setupWithViewPager(mViewPager)
     }
 
     override fun onBackPressed() {
