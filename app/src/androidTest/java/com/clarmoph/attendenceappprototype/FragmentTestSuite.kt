f3c4637fcf96f71631ec06d9c30500537fdfe61f
Murray <murrayde17@students.ecu.edu>
2019-09-09 21:02:00 -0400

    add time fragment

---
+++ app/src/androidTest/java/com/clarmoph/attendenceappprototype/FragmentTestSuite.kt
@@ -0,0 +1,47 @@
+package com.clarmoph.attendenceappprototype
+
+
+import android.app.DatePickerDialog
+import androidx.fragment.app.testing.launchFragment
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.clarmoph.attendenceappprototype.views.AdjustCheckInTimeFragment
+import com.clarmoph.attendenceappprototype.views.AdjustProximityFragment
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class FragmentTestSuite {
+
+    @Test
+    fun testCheckInTimeFragmentIsDisplayed() {
+        launchFragmentInContainer<AdjustCheckInTimeFragment>()
+
+        onView(withId(R.id.checkdate_dialog))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testAdjustProximityFragmentIsDisplayed() {
+        launchFragmentInContainer<AdjustProximityFragment>()
+
+        onView(withId(R.id.adjust_proximity))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testCheckInTimeSetDateButtonIsInvisible() {
+        launchFragmentInContainer<AdjustCheckInTimeFragment>()
+
+        onView(withId(R.id.btn_checkin_date))
+            .perform(click())
+        onView(withId(R.id.btn_checkin_date))
+            .check(matches(isDisplayed()))
+    }
+
+}
--- app/src/main/java/com/clarmoph/attendenceappprototype/views/AdjustCheckInTimeFragment.kt
+++ app/src/main/java/com/clarmoph/attendenceappprototype/views/AdjustCheckInTimeFragment.kt
@@ -1,23 +1,38 @@
 package com.clarmoph.attendenceappprototype.views
 
+import android.app.DatePickerDialog
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.DatePicker
 import com.clarmoph.attendenceappprototype.R
 import com.clarmoph.attendenceappprototype.viewmodels.CheckInTimeViewModel
 import kotlinx.android.synthetic.main.adjust_check_in_time_fragment.*
 
-class AdjustCheckInTimeFragment : Fragment() {
+
+class AdjustCheckInTimeFragment : Fragment(), DatePickerDialog.OnDateSetListener {
 
     companion object {
         fun newInstance() = AdjustCheckInTimeFragment()
     }
 
+    private val MONTHS = arrayOf(
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+    )
     private lateinit var viewModel: CheckInTimeViewModel
     private var TAG = AdjustCheckInTimeFragment::class.java.name
 
@@ -32,11 +47,25 @@ class AdjustCheckInTimeFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(CheckInTimeViewModel::class.java)
         btn_checkin_date.setOnClickListener { showDateFragment() }
-        // TODO: Use the ViewModel
     }
 
     private fun showDateFragment() {
-        Log.d(TAG, "Set date button clicked")
+        val datePickerDialog = DatePickerDialog(activity!!)
+        datePickerDialog.setOnDateSetListener(this)
+        datePickerDialog.show()
+    }
+
+
+    override fun onDateSet(datePicker: DatePicker?, years: Int, months: Int, days: Int) {
+        // TODO : Update date property LiveData
+        btn_checkin_date.visibility = View.INVISIBLE
+        from_date.visibility = View.VISIBLE
+        from_date_calender.visibility = View.VISIBLE
+        from_date_calender.text = "${MONTHS[months]}, $days @ 9:30 AM"
+
+        to_date.visibility = View.VISIBLE
+        to_date_calender.visibility = View.VISIBLE
+        to_date_calender.text = "${MONTHS[months]}, $days @ 9:30 AM"
     }
 
 }
