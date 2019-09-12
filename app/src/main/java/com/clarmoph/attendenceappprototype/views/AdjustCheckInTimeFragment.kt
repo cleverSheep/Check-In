a72b961792401ce48410d969648d6164c7fff91a
cleverSheep <clarmoph1029@gmail.com>
2019-09-12 13:52:48 -0400

    extract string resources

--- app/src/main/java/com/clarmoph/attendenceappprototype/views/AdjustCheckInTimeFragment.kt
+++ app/src/main/java/com/clarmoph/attendenceappprototype/views/AdjustCheckInTimeFragment.kt
@@ -1,13 +1,13 @@
 package com.clarmoph.attendenceappprototype.views
 
 import android.app.DatePickerDialog
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.DatePicker
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.clarmoph.attendenceappprototype.R
 import com.clarmoph.attendenceappprototype.viewmodels.CheckInTimeViewModel
 import kotlinx.android.synthetic.main.adjust_check_in_time_fragment.*
@@ -58,6 +58,10 @@ class AdjustCheckInTimeFragment : Fragment(), DatePickerDialog.OnDateSetListener
 
     override fun onDateSet(datePicker: DatePicker?, years: Int, months: Int, days: Int) {
         // TODO : Update date property LiveData
+        swapViewVisibility(years, months, days)
+    }
+
+    private fun swapViewVisibility(years: Int, months: Int, days: Int) {
         btn_checkin_date.visibility = View.INVISIBLE
         from_date.visibility = View.VISIBLE
         from_date_calender.visibility = View.VISIBLE
---
+++ app/src/main/java/com/clarmoph/attendenceappprototype/views/ProximityCustomView.kt
@@ -0,0 +1,15 @@
+package com.clarmoph.attendenceappprototype.views
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+
+class ProximityCustomView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
+
+}
