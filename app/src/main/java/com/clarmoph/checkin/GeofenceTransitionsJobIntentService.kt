f84757d97a520debd46f748fc253013a009bba69
cleverSheep <clarmoph1029@gmail.com>
2019-10-30 11:15:23 -0400

    setup location notification

--- app/src/main/java/com/clarmoph/checkin/GeofenceTransitionsJobIntentService.kt
+++ app/src/main/java/com/clarmoph/checkin/GeofenceTransitionsJobIntentService.kt
@@ -1,19 +1,175 @@
 package com.clarmoph.checkin
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
+import android.text.TextUtils
+import android.util.Log
 import androidx.core.app.JobIntentService
+import androidx.core.app.NotificationCompat
+import com.clarmoph.checkin.utils.GeofenceErrorMessages
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingEvent
 
+/**
+ * Listener for geofence transition changes.
+ *
+ * Receives geofence transtion events from Location Services in an Intent containing
+ * the transition type and geofence id(s) that triggered the transition. Creates a notification
+ * as the output.
+ */
 class GeofenceTransitionsJobIntentService : JobIntentService() {
 
     companion object {
-        private const val JOB_ID = 274
 
+        private const val JOB_ID = 573
+
+        private const val TAG = "GeofenceTransitionIS"
+
+        private const val CHANNEL_ID = "channel_01"
+
+        /**
+         * Convenience method for enqueuing work in to this service
+         */
         fun enqueueWork(context: Context, intent: Intent) {
             enqueueWork(context, GeofenceTransitionsJobIntentService::class.java, JOB_ID, intent)
         }
+
     }
 
-    override fun onHandleWork(intent: Intent) {}
+    override fun onHandleWork(intent: Intent) {
+        val geofencingEvent = GeofencingEvent.fromIntent(intent)
+        if (geofencingEvent.hasError()) {
+            val errorMessage = GeofenceErrorMessages.getErrorString(this, geofencingEvent.errorCode)
+            Log.e(TAG, errorMessage)
+            return
+        }
+
+        // Get the transition type.
+        val geofenceTranstion = geofencingEvent.geofenceTransition
+
+        // Test that the reported transition was of interest.
+        if (geofenceTranstion == Geofence.GEOFENCE_TRANSITION_ENTER || geofenceTranstion == Geofence.GEOFENCE_TRANSITION_EXIT) {
+
+            // List the geofences that were triggered. A single event can trigger multiple geofences.
+            val triggeringGeofences = geofencingEvent.triggeringGeofences
+
+            // Get the transition details as a String.
+            val geofenceTransitionDetails =
+                getGeofenceTransitionDetails(geofenceTranstion, triggeringGeofences)
+
+            // Send notifications and log the transition details
+            sendNotifications(geofenceTransitionDetails)
+            Log.i(TAG, geofenceTransitionDetails)
+        } else {
+            // Log the error.
+            Log.e(TAG, getString(R.string.geofence_transition_invalid_type, geofenceTranstion))
+        }
+    }
+
+    /**
+     * Gets transition details and returns them as a formatted string.
+     *
+     * @param geofenceTransition    The ID of the geofence transition.
+     * @param triggeringGeofences   The geofence(s) triggered.
+     * @return                      The transition details formatted as String.
+     */
+    private fun getGeofenceTransitionDetails(
+        geofenceTransition: Int,
+        triggeringGeofences: List<Geofence>
+    ): String {
+
+        val geofenceTransitionString = getTransitionString(geofenceTransition)
+
+        // Get the IDs of each geofence that was triggered.
+        val triggeringGeofencesIDList: ArrayList<String> = ArrayList()
+        for (geofence in triggeringGeofences) {
+            triggeringGeofencesIDList.add(geofence.requestId)
+        }
+        val triggeringGeofencesIDsString = TextUtils.join(", ", triggeringGeofencesIDList)
+
+        return "$geofenceTransitionString: $triggeringGeofencesIDsString"
+    }
+
+    private fun sendNotifications(notificationDetails: String) {
+        // Get an instance of the Notification manager
+        val mNotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Android O requires a Notification Channel.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name: CharSequence = getString(R.string.app_name)
+            // Create the channel for the notification
+            val mChannel =
+                NotificationChannel(CHANNEL_ID, name, NotificationManager.IMPORTANCE_DEFAULT)
+
+            // Set the Notification Channel for the Notification Manager
+            mNotificationManager.createNotificationChannel(mChannel)
+        }
+
+        // Create an explicit content Intent that starts the main Activity.
+        val notificationIntent = Intent(applicationContext, MainActivity::class.java)
+
+        // Construct a task stack.
+        val stackBuilder = TaskStackBuilder.create(this)
+
+        // Add the main Activity to the task stack as the parent.
+        stackBuilder.addParentStack(MainActivity::class.java)
+
+        // Push the content Intent onto the stack.
+        stackBuilder.addNextIntent(notificationIntent)
+
+        // Get a PendingIntent containing the entire back stack
+        val notificationPendingIntent =
+            stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        // Get a notification builder that's compatible with the platform versions >= 4
+        val builder: NotificationCompat.Builder = NotificationCompat.Builder(this)
+
+        // Define the notification settings.
+        builder.setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setLargeIcon(
+                BitmapFactory.decodeResource(
+                    resources,
+                    R.drawable.ic_launcher_foreground
+                )
+            )
+            .setColor(Color.BLUE)
+            .setContentTitle(notificationDetails)
+            .setContentText(getString(R.string.geofence_transition_notification_text))
+            .setContentIntent(notificationPendingIntent)
+
+        // Set the Channel ID for Android O
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder.setChannelId(CHANNEL_ID)
+        }
+
+        // Dismiss notification when the user touches it
+        builder.setAutoCancel(true)
+
+        // Issue the notification
+        mNotificationManager.notify(0, builder.build())
+    }
+
+    /**
+     * Maps geofence transition types to their human-readable equivalents.
+     *
+     * @param transitionType    A transition type constant defined in Geofence
+     * @return                  A String indicating the type of transition
+     */
+    private fun getTransitionString(transitionType: Int): String {
+        return when (transitionType) {
+            Geofence.GEOFENCE_TRANSITION_ENTER -> getString(R.string.geofence_transition_entered)
+            Geofence.GEOFENCE_TRANSITION_EXIT -> getString(R.string.geofence_transition_exited)
+            else -> getString(R.string.unknown_geofence_transition)
+
+        }
+    }
 
 }
