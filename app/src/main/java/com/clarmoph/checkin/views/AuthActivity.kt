18110258f9b0d318d4f5ddee84d8fde20780a1a0
cleverSheep <clarmoph1029@gmail.com>
2019-11-07 19:02:52 -0500

    implement status design

---
+++ app/src/main/java/com/clarmoph/checkin/views/AuthActivity.kt
@@ -0,0 +1,13 @@
+package com.clarmoph.checkin.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.clarmoph.checkin.R
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.auth_main)
+    }
+}
